14ba8054a2bdba5c9c8fc13ebf710cc741d54603
salmantariqmirza <stariqmi@gmail.com>
2013-12-16 23:19:41 -0800

    similar text possible bug intro introduction in script

--- duplicateRemover.rb
+++ duplicateRemover.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'spreadsheet'
+require 'similar_text'
 
 source_xls = Spreadsheet.open 'MLS Cash Buyers/main_properties.xls'
 source_sheet = source_xls.worksheet 0
@@ -8,9 +9,17 @@ headers = all_headers[0...4] << "Same" << all_headers[5] << "# Owns"
 
 def checkUniqueness row, unique_rows
 	unique_rows.each do |ur|
-		return false if row[0] == ur[0]
+		check = row[0] || ""
+		puts "Comparing '#{check}' with  '#{ur[0]}'"
+		if check == ""
+			puts "No mailing address ... "
+			return false
+		elsif check.similar(ur[0]) > 65
+			puts "#{ur[0]} is similar to #{check}"
+			return false
+		end
 	end
-	puts "Uniqueness confirmed."
+	puts "Uniqueness confirmed.\n"
 	return true
 end
 
@@ -18,7 +27,7 @@ unique_rows = []
 
 source_sheet.each 1 do |row|
 	info = row[0..4] << row[5]
-	puts "Checking '#{info[0]}' for uniqueness ... "
+	puts "\n\tChecking '#{info[0]}' for uniqueness ... "
 	unique_rows << info if checkUniqueness info, unique_rows
 end
 
